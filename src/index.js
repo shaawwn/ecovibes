ce085c7bb56f97365fa8ce9be8f536a430779784
Shawn Lentz <shawn.lentz@gmail.com>
2024-07-10 12:57:27 -1000

    added styling to nav

--- src/index.js
+++ src/index.js
@@ -5,13 +5,54 @@ import {Button} from './components'
 
 console.log("Loading scripts..")
 
+
 document.addEventListener("DOMContentLoaded", () => {
+    loadElements()
     const title = document.createElement('h1')
     title.innerText = 'Hello, world'
-
-    const button = new Button('Get early access')
-
     document.body.appendChild(title)
-    document.body.appendChild(button)
 })
+
+function loadElements() {
+    addLogo()
+    checkMobile()
+    handleScreenResize()
+}
+
+function addLogo() {
+    const logo = document.getElementById('nav-logo')
+    logo.src = Logo
+}
+
+function checkMobile() {
+    const screenWidth = window.innerWidth
+    if(screenWidth < 764) {
+        hideNavLinks()
+    }
+}
+
+function handleScreenResize() {
+    window.addEventListener('resize', () => {
+        const screenWidth = window.innerWidth
+        if(screenWidth < 764) {
+            hideNavLinks()
+        } else if(screenWidth > 764) {
+            showNavLinks()
+        }
+    })
+}
+
+function hideNavLinks() {
+    const navLinks = document.getElementsByClassName('navbar__links')[0]
+    const hamburger = document.getElementById('navbar-menu')
+    navLinks.style.display = "none"
+    hamburger.style.display = 'block'
+}
+
+function showNavLinks() {
+    const navLinks = document.getElementsByClassName('navbar__links')[0]
+    const hamburger = document.getElementById('navbar-menu')
+    navLinks.style.display = "flex"
+    hamburger.style.display = 'none'
+}
 
